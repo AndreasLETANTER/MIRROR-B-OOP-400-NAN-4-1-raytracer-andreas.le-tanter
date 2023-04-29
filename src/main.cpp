276a8367d43508419f2a05b3ffe0f80c637c35e4
AndreasLETANTER <andreas.le-tanter@epitech.eu>
2023-04-29 19:11:55 +0200

    [FIX]: camera position

--- src/main.cpp
+++ src/main.cpp
@@ -21,12 +21,12 @@ int main(void)
 {
     RayTracer::Camera cam;
     RayTracer::Renderer renderer;
-    RayTracer::Sphere sphere(Math::Point3D(0.5, -5, 0), 0.3, Math::Vector3D(153, 0, 153));
-    RayTracer::Sphere sphere2(Math::Point3D(-0.5, -5, 0), 0.3, Math::Vector3D(255, 255, 0));
-    RayTracer::Sphere sphere3(Math::Point3D(0, -5, 0.5), 0.3, Math::Vector3D(178, 255, 102));
-    RayTracer::Sphere sphere4(Math::Point3D(0, -5, -0.5), 0.3, Math::Vector3D(255, 153, 153));
+    RayTracer::Sphere sphere(Math::Point3D(0.5, 0.5, 0), 0.3, Math::Vector3D(153, 0, 153));
+    RayTracer::Sphere sphere2(Math::Point3D(-0.5, 0.5, 0), 0.3, Math::Vector3D(255, 255, 0));
+    RayTracer::Sphere sphere3(Math::Point3D(0.5, -0.5, 0), 0.3, Math::Vector3D(178, 255, 102));
+    RayTracer::Sphere sphere4(Math::Point3D(-0.5, -0.5, 0), 0.3, Math::Vector3D(255, 153, 153));
     RayTracer::AmbiantLight ambiantLight(0.2);
-    RayTracer::DirectionalLight directionalLight(Math::Vector3D(1, -1, 1));
+    RayTracer::DirectionalLight directionalLight(Math::Vector3D(1, 1, 1.5));
     std::vector<std::shared_ptr<RayTracer::IObjects>> objects;
     std::vector<std::shared_ptr<RayTracer::ILights>> lights;
 
@@ -36,8 +36,8 @@ int main(void)
     objects.push_back(std::make_shared<RayTracer::Sphere>(sphere4));
     lights.push_back(std::make_shared<RayTracer::AmbiantLight>(ambiantLight));
     lights.push_back(std::make_shared<RayTracer::DirectionalLight>(directionalLight));
-    cam.m_origin = Math::Point3D(0, 5, 0);
-    cam.m_screen = Rectangle3D(Math::Point3D(-0.5, 0, -0.5), Math::Vector3D(1, 0, 0), Math::Vector3D(0, 0, 1));
+    cam.m_origin = Math::Point3D(0, 0, -5);
+    cam.m_screen = Rectangle3D(Math::Point3D(-0.25, -0.25, -4), Math::Vector3D(0.5, 0, 0), Math::Vector3D(0, 0.5, 0));
     renderer.setCamera(cam);
     renderer.setWidth(WIDTH);
     renderer.setHeight(HEIGHT);

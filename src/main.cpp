5ecdf67b15f5c2b14e60946f8b6bb032cc263974
AndreasLETANTER <andreas.le-tanter@epitech.eu>
2023-04-29 16:56:02 +0200

    [ADD]: multiple sphere rendered with light for example

--- src/main.cpp
+++ src/main.cpp
@@ -21,13 +21,19 @@ int main(void)
 {
     RayTracer::Camera cam;
     RayTracer::Renderer renderer;
-    RayTracer::Sphere sphere(Math::Point3D(0, 0, 0), 0.15, Math::Vector3D(255, 0, 0));
+    RayTracer::Sphere sphere(Math::Point3D(0.5, -5, 0), 0.3, Math::Vector3D(153, 0, 153));
+    RayTracer::Sphere sphere2(Math::Point3D(-0.5, -5, 0), 0.3, Math::Vector3D(255, 255, 0));
+    RayTracer::Sphere sphere3(Math::Point3D(0, -5, 0.5), 0.3, Math::Vector3D(178, 255, 102));
+    RayTracer::Sphere sphere4(Math::Point3D(0, -5, -0.5), 0.3, Math::Vector3D(255, 153, 153));
     RayTracer::AmbiantLight ambiantLight(0.2);
     RayTracer::DirectionalLight directionalLight(Math::Vector3D(1, -1, 1));
     std::vector<std::shared_ptr<RayTracer::IObjects>> objects;
     std::vector<std::shared_ptr<RayTracer::ILights>> lights;
 
     objects.push_back(std::make_shared<RayTracer::Sphere>(sphere));
+    objects.push_back(std::make_shared<RayTracer::Sphere>(sphere2));
+    objects.push_back(std::make_shared<RayTracer::Sphere>(sphere3));
+    objects.push_back(std::make_shared<RayTracer::Sphere>(sphere4));
     lights.push_back(std::make_shared<RayTracer::AmbiantLight>(ambiantLight));
     lights.push_back(std::make_shared<RayTracer::DirectionalLight>(directionalLight));
     cam.m_origin = Math::Point3D(0, 5, 0);
@@ -35,7 +41,7 @@ int main(void)
     renderer.setCamera(cam);
     renderer.setWidth(WIDTH);
     renderer.setHeight(HEIGHT);
-    renderer.setMissColor(Math::Vector3D(0, 0, 0));
+    renderer.setMissColor(Math::Vector3D(105, 105, 105));
     renderer.setObjects(objects);
     renderer.setLights(lights);
     renderer.renderScene();

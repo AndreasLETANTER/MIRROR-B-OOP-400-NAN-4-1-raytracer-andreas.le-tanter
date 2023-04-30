4f106d18f9c26cfc65bd2dd9a5909e9bd895f330
AndreasLETANTER <andreas.le-tanter@epitech.eu>
2023-04-30 23:44:44 +0200

    [MODIFY]: the camera for letting it work like the config file, but without the fov

--- src/RayTracer/Camera/Camera.cpp
+++ src/RayTracer/Camera/Camera.cpp
@@ -17,5 +17,46 @@
 */
 RayTracer::Ray RayTracer::Camera::rayAt(double u, double v)
 {
-    return (RayTracer::Ray(m_origin, m_screen.pointAt(u, v) - m_origin));
+    return (RayTracer::Ray(m_position, m_screen.pointAt(u, v) - m_position));
+}
+
+/**
+ * @brief set the position of the camera
+ * @param t_origin 
+*/
+void RayTracer::Camera::setPosition(Math::Point3D t_origin)
+{
+    m_position = t_origin;
+}
+
+/**
+ * @brief set the screen of the camera
+ * @details the screen is defined by its fov and its resolution
+ * @param t_fov 
+*/
+void RayTracer::Camera::setScreen(double t_fov)
+{
+    (void) t_fov;
+    m_screen = Rectangle3D(Math::Point3D(m_position.m_x_component - 0.25, m_position.m_y_component - 0.25, -4), Math::Vector3D(0.5, 0, 0), Math::Vector3D(0, 0.5, 0));
+}
+
+/**
+ * @brief set the resolution of the camera
+ * @param t_width 
+ * @param t_height 
+*/
+void RayTracer::Camera::setResolution(int t_width, int t_height)
+{
+    m_width = t_width;
+    m_height = t_height;
+}
+
+/**
+ * @brief rotate the screen of the camera
+ * @details rotate the screen of the camera by the given rotation
+ * @param t_rotation 
+*/
+void RayTracer::Camera::rotateScreen(Math::Vector3D t_rotation)
+{
+    (void) t_rotation;
 }
--- src/RayTracer/Camera/Camera.hpp
+++ src/RayTracer/Camera/Camera.hpp
@@ -10,6 +10,7 @@
 #include "../../Math/Vector3D/Vector3D.hpp"
 #include "../../Math/Rectangle3D/Rectangle3D.hpp"
 #include "../Ray/Ray.hpp"
+#include <tuple>
 
 /**
  * @brief Camera class, used to create a camera in 3D space
@@ -21,11 +22,18 @@ namespace RayTracer
         public:
             Camera(void) = default;
             ~Camera(void) = default;
-            Math::Point3D m_origin;
-            Rectangle3D m_screen;
-            RayTracer::Ray rayAt(double u, double v);
+            void setPosition(Math::Point3D t_origin);
+            void setScreen(double t_fov);
+            void setResolution(int t_width, int t_height);
+            void rotateScreen(Math::Vector3D t_rotation);
+            std::tuple<int, int> getResolution(void) { return (std::make_tuple(m_width, m_height)); };
+            RayTracer::Ray rayAt(double t_u, double t_v);
 
         protected:
         private:
+            Math::Point3D m_position;
+            Rectangle3D m_screen;
+            int m_width;
+            int m_height;
     };
 }

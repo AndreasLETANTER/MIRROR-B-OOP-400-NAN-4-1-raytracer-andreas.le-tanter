c322d47f6c8c9a085d921973727c1bf161553e85
AndreasLETANTER <andreas.le-tanter@epitech.eu>
2023-05-03 21:21:56 +0200

    [ADD]: basic main with sphere at the center and a directionnal light

--- src/main.cpp
+++ src/main.cpp
@@ -22,29 +22,15 @@ int main(void)
 {
     RayTracer::Camera cam;
     RayTracer::Renderer renderer;
-    RayTracer::Sphere sphere(Math::Point3D(0.5, 0.5, 0), 0.3, Math::Vector3D(153, 0, 153));
-    RayTracer::Sphere sphere2(Math::Point3D(-0.5, 0.5, 0), 0.3, Math::Vector3D(255, 255, 0));
-    RayTracer::Sphere sphere3(Math::Point3D(0.5, -0.5, 0), 0.3, Math::Vector3D(178, 255, 102));
-    RayTracer::Sphere sphere4(Math::Point3D(-0.5, -0.5, 0), 0.3, Math::Vector3D(255, 153, 153));
-    RayTracer::Plane plane(Math::Point3D(0., 0.9, 0.), Math::Vector3D(0., -1., 0.), Math::Vector3D(18, 164, 139));
-    RayTracer::Plane plane2(Math::Point3D(0.9, 0., 0.), Math::Vector3D(-1., 0., 0.), Math::Vector3D(18, 164, 139));
-    RayTracer::Plane plane3(Math::Point3D(0., -0.9, 0.), Math::Vector3D(0., 1., 0.), Math::Vector3D(18, 164, 139));
-    RayTracer::Plane plane4(Math::Point3D(-0.9, 0., 0.), Math::Vector3D(1., 0., 0.), Math::Vector3D(18, 164, 139));
-    RayTracer::Plane plane5(Math::Point3D(0., 0., -0.15), Math::Vector3D(0., 0., 1.), Math::Vector3D(18, 164, 139));
+    RayTracer::Sphere sphere(Math::Point3D(0, 0, 0), 0.3, Math::Vector3D(153, 0, 153));
+    RayTracer::Plane plane(Math::Point3D(0., 0.4, 0.), Math::Vector3D(0., -1., 0.), Math::Vector3D(18, 164, 139));
     RayTracer::AmbiantLight ambiantLight(0.2);
     RayTracer::DirectionalLight directionalLight(Math::Vector3D(-1, 1, 1.5));
     std::vector<std::shared_ptr<RayTracer::IObjects>> objects;
     std::vector<std::shared_ptr<RayTracer::ILights>> lights;
 
     objects.push_back(std::make_shared<RayTracer::Sphere>(sphere));
-    objects.push_back(std::make_shared<RayTracer::Sphere>(sphere2));
-    objects.push_back(std::make_shared<RayTracer::Sphere>(sphere3));
-    objects.push_back(std::make_shared<RayTracer::Sphere>(sphere4));
     objects.push_back(std::make_shared<RayTracer::Plane>(plane));
-    objects.push_back(std::make_shared<RayTracer::Plane>(plane2));
-    objects.push_back(std::make_shared<RayTracer::Plane>(plane3));
-    objects.push_back(std::make_shared<RayTracer::Plane>(plane4));
-    objects.push_back(std::make_shared<RayTracer::Plane>(plane5));
     lights.push_back(std::make_shared<RayTracer::AmbiantLight>(ambiantLight));
     lights.push_back(std::make_shared<RayTracer::DirectionalLight>(directionalLight));
     cam.setResolution(WIDTH, HEIGHT);

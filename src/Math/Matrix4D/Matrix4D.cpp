45835b5c8171091badbcb723810b49b22369c598
Yamao Cuzou <yamao.cuzou@epitech.eu0>
2023-05-08 20:00:49 +0200

    [ADD]: get_determinant, inverse, getcofactor, get_determiant3x3 functions to calcul the matrix inversed

--- src/Math/Matrix4D/Matrix4D.cpp
+++ src/Math/Matrix4D/Matrix4D.cpp
@@ -15,39 +15,121 @@ Math::Matrix4D::Matrix4D(void)
 {
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
-            this->_m_elements[i][j] = 0.0f;
+            this->_matrix[i][j] = 0.0f;
         }
     }
 }
 
+/**
+ * @brief Construct a new Matrix4D:: Matrix4D object
+ * @details set the float matrix _matrix value to the given parameters
+ * @param elements
+*/
 Math::Matrix4D::Matrix4D(double elements[4][4])
 {
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
-            this->_m_elements[i][j] = elements[i][j];
+            this->_matrix[i][j] = elements[i][j];
         }
     }
 }
 
-double &Math::Matrix4D::get_elements(int row, int col)
+/**
+ * @brief get the one element of the matrix
+ * @param row
+ * @param col
+ * @return double
+*/
+double Math::Matrix4D::get_elements(int row, int col)
 {
-    return _m_elements[row][col];
+    return _matrix[row][col];
 }
 
+/**
+ * @brief inverse all the elements of the matrix with the deteminant and the Cofactor
+ * @return Math::Matrix4D
+*/
 Math::Matrix4D Math::Matrix4D::inverse(void)
 {
     Math::Matrix4D result;
+    double det = get_determinant();
+    double invDet = 0.0;
+    double cofactor = 0.0;
 
-    float det = get_determinant();
     if (det == 0.0f) {
         return result;
     }
-    float invDet = 1.0f / det;
+    invDet = 1.0f / det;
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
-            float cofactor = getCofactor(i, j);
-            result._m_elements[j][i] = cofactor * invDet;
+            cofactor = getCofactor(i, j);
+            result._matrix[j][i] = cofactor * invDet;
         }
     }
     return result;
+}
+
+/**
+ * @brief calcul the determinant of the matrix and return it
+ * @return double
+*/
+double Math::Matrix4D::get_determinant(void)
+{
+    double det = 0.0;
+    double cofactor = 0.0;
+
+    for (int col = 0; col < 4; col++) {
+        cofactor = getCofactor(0, col);
+        det += _matrix[0][col] * cofactor;
+    }
+    return (det);
+}
+
+/**
+ * @brief calcul the cofactor of the matrix and return it
+ * @param row
+ * @param col
+ * @return double
+*/
+double Math::Matrix4D::getCofactor(int row, int col)
+{
+    double subMatrix[3][3];
+    int subRow = 0;
+    int subCol = 0;
+    double sign = 0.0;
+    double cofactor = 0.0;
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (i != row && j != col) {
+                subMatrix[subRow][subCol] = _matrix[i][j];
+                subCol++;
+                if (subCol == 3) {
+                    subCol = 0;
+                    subRow++;
+                }
+            }
+        }
+    }
+    sign = ((row + col) % 2 == 0) ? 1.0f : -1.0f;
+    cofactor = sign * get_determinant3x3(subMatrix);
+    return (cofactor);
+}
+
+/**
+ * @brief calcul the determinant of a matrix 3x3 and return it
+ * @param matrix
+ * @return double
+*/
+double Math::Matrix4D::get_determinant3x3(double matrix[3][3])
+{
+    float det = 0.0f;
+
+    det += matrix[0][0] * (matrix[1][1] * matrix[2][2]
+    - matrix[1][2] * matrix[2][1]);
+    det -= matrix[0][1] * (matrix[1][0] * matrix[2][2]
+    - matrix[1][2] * matrix[2][0]);
+    det += matrix[0][2] * (matrix[1][0] * matrix[2][1]
+    - matrix[1][1] * matrix[2][0]);
+    return (det);
 }
--- src/Math/Matrix4D/Matrix4D.hpp
+++ src/Math/Matrix4D/Matrix4D.hpp
@@ -17,17 +17,18 @@ namespace Math
     class Matrix4D {
         public:
             Matrix4D(void);
-            Matrix4D(double element[4][4]);
+            Matrix4D(double matrix[4][4]);
             Matrix4D(const Matrix4D &t_other) = default;
             Matrix4D &operator=(const Matrix4D &t_other) = default;
             Matrix4D(Matrix4D &&t_other) = default;
             Matrix4D &operator=(Matrix4D &&t_other) = default;
             ~Matrix4D(void) = default;
-            double &get_elements(int row, int col);
+            double get_elements(int row, int col);
             Math::Matrix4D inverse(void);
             double get_determinant(void);
             double getCofactor(int row, int col);
+            double get_determinant3x3(double matrix[3][3]);
         public:
-            double _m_elements[4][4];
+            double _matrix[4][4];
     };
 }

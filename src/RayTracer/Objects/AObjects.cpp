bede767e7e0e9bf3b09c81aebab6dcfafcf12cf1
hugon <hugo.noury@epitech.eu>
2023-05-08 21:11:34 +0200

    [SIMPLIFY]: translate method

--- src/RayTracer/Objects/AObjects.cpp
+++ src/RayTracer/Objects/AObjects.cpp
@@ -8,7 +8,5 @@
 
 void RayTracer::AObjects::translate(Math::Point3D m_origin)
 {
-    this->m_position.m_x_component += m_origin.m_x_component;
-    this->m_position.m_y_component += m_origin.m_y_component;
-    this->m_position.m_z_component += m_origin.m_z_component;
+    m_position = m_origin;
 }
